6482bcb939a3458c84e1713bda4009c02de0e6f6
Alex <alexanderpgh333@gmail.com>
2018-01-31 14:19:19 -0600

    created algorithm to solve simple mazes.

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -17,30 +17,54 @@ public class SolveMaze {
      */
 
     public static void main(final String[] unused) {
-        /*
-         * Create a new 10 x 10 maze. Feel free to change these values.
-         */
-        Maze maze = new Maze(10, 10);
 
-        /*
-         * Pick (0, 0), the bottom left corner, as the starting point.
-         * Put the end in the top right corner.
-         */
-        maze.startAtZero();
-        maze.endAtTopRight();
+        int total = 0; int successes = 0; int average = 0; int steps = 0;
+        while(total<50){
+            steps = 0;
 
-        /*
-         * You should be able to solve a 10 x 10 maze in (far fewer than) 1000 steps.
-         * Feel free to adjust this number if you experiment with other mazes.
-         */
-        for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+            /*
+             * Create a new 10 x 10 maze. Feel free to change these values.
+             */
+            //System.out.println("Generating Maze...");
+            Maze maze = new Maze(10, 10);
+
+            /*
+             * Pick (0, 0), the bottom left corner, as the starting point.
+             * Put the end in the top right corner.
+             */
+            maze.startAtZero();
+            maze.endAtTopRight();
+
+            /*
+             * You should be able to solve a 10 x 10 maze in (far fewer than) 1000 steps.
+             * Feel free to adjust this number if you experiment with other mazes.
+             */
+            System.out.println("Running...");
+            for (int step = 0; step < 10000 && maze.isFinished()==false; step++) {
+                // Implement your maze solving algorithm here
+            steps++;
+
+                maze.move();
+                maze.turnLeft();
+                while(!maze.canMove()) {
+                    maze.turnRight();
+                }
+
+
+            }
+
+            if (maze.isFinished()) {
+                System.out.println("Success");
+                successes++;
+            } else {
+                System.out.println("Failed");
+            }
+            // Updates the average.
+            average += steps;
+            total++;
         }
-
-        if (maze.isFinished()) {
-            System.out.println("You solved the maze!");
-        } else {
-            System.out.println("Try again!");
-        }
+        average /= total;
+        System.out.println("\n\nSucceeded " + successes + "/" + total + ".");
+        System.out.println("Average completion time is " + average + " steps.");
     }
 }
